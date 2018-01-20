1a4f4d75fb5f4d41c0b809c283d732cdde8c384f
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-20 22:55:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+var a,b,n;
+
+//Part 1
+function add(a,b){
+  return a + b ;
+}add (9,2);
+
+//Part 2
+function subtract(a,b){
+  return a-b;
+}subtract(2,7);
+
+//Part 3
+function multiply(a,b){
+  return a * b;
+}multiply(4,2);
+
+//Part 4
+function divide(a,b){
+  return a/b;
+}divide(a,b);
+
+//Part 5
+function inc(n){
+  return n+=1;
+}inc(4156);
+
+//Part 6
+function dec(n){
+  return n-=1;
+}dec(28)
+
+//Part 7
+function makeInt(n){
+  return parseInt('n', 10);
+}makeInt("47");
